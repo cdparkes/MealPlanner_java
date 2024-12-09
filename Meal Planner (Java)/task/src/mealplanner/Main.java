2101b2da506752c08996b34440eb55a5b866e0d9
chris <christopher.david.parkes@gmail.com>
2024-12-09 09:56:19 +0100

    Refactor to use TableManager for table creation
    Introduced a new TableManager instance in Main for managing table creation, replacing direct calls to ConnectionManager. This improves code organization and separates responsibilities, leading to a clearer structure in database handling. Adjusted indentation for better readability.

--- Meal Planner (Java)/task/src/mealplanner/Main.java
+++ Meal Planner (Java)/task/src/mealplanner/Main.java
@@ -1,34 +1,37 @@
 package mealplanner;
 
 import mealplanner.dbHandler.ConnectionManager;
+import mealplanner.dbHandler.TableManager;
 
 public class Main {
 
-  public ConnectionManager dbConnection = new ConnectionManager();
+    public ConnectionManager dbConnection = new ConnectionManager();
+    public TableManager tbManager = new TableManager();
 
-  public static void main(String[] args) {
-    ConnectionManager.getConnection();
+    public static void main(String[] args) {
+        Main app = new Main();
 
-    ConnectionManager.createTable("meals",
-            "category", "VARCHAR(50)",
-            "meal", "VARCHAR(50)",
-            "meal_id", "INTEGER");
+        ConnectionManager.getConnection();
 
-    ConnectionManager.createTable("ingredients",
-            "ingredient", "VARCHAR(50)",
-            "ingredient_id", "INTEGER",
-            "meal_id", "INTEGER");
+        app.tbManager.createTable("meals",
+                "category", "VARCHAR(50)",
+                "meal", "VARCHAR(50)",
+                "meal_id", "INTEGER");
 
-    Main app = new Main();
-    if (app.dbConnection.isConnectionEstablished()) {
-      System.out.println("Connection successfully established.");
-    } else {
-      System.out.println("Failed to establish connection.");
+        app.tbManager.createTable("ingredients",
+                "ingredient", "VARCHAR(50)",
+                "ingredient_id", "INTEGER",
+                "meal_id", "INTEGER");
+
+        if (app.dbConnection.isConnectionEstablished()) {
+            System.out.println("Connection successfully established.");
+        } else {
+            System.out.println("Failed to establish connection.");
+        }
+
+        while (Menu.inputMenu()) {
+        }
+
+        ConnectionManager.closeConnection();
     }
-
-    while (Menu.inputMenu()) {
-    }
-
-    ConnectionManager.closeConnection();
-  }
 }
