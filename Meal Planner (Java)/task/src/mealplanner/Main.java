4c686f87f563818c94ca5feb0cf01fdfd4b98695
Christopher Parkes <84539407+cdparkes@users.noreply.github.com>
2024-12-16 09:49:47 +0100

    Fixing Problems
    Fixing problems that came up when running on a different computer.

--- Meal Planner (Java)/task/src/mealplanner/Main.java
+++ Meal Planner (Java)/task/src/mealplanner/Main.java
@@ -9,26 +9,26 @@ import java.sql.Connection;
 
 public class Main {
 
-    public final ConnectionManager dbConnection = new ConnectionManager();
-    public final TableManager tbManager = new TableManager();
-    public static final Logger logger = LoggerFactory.getLogger(Main.class);
+  public final ConnectionManager dbConnection = new ConnectionManager();
+  public final TableManager tbManager = new TableManager();
+  public static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
-        Main app = new Main();
+  public static void main(String[] args) {
+    Main app = new Main();
 
-        Connection connection = ConnectionManager.getConnection();
+    Connection connection = ConnectionManager.getConnection();
 
-        app.tbManager.createTable();
-        if (app.dbConnection.isConnectionEstablished()) {
+    app.tbManager.createTable();
+    if (app.dbConnection.isConnectionEstablished()) {
 //            logger.info("Connection successfully established.");
-            Menu menu = new Menu(connection);
-            while (true) {
-                if (!menu.inputMenu()) break;
-            }
-        } else {
-            logger.error("Failed to establish connection.");
-        }
-
-        ConnectionManager.closeConnection();
+      Menu menu = new Menu(connection);
+      while (true) {
+        if (!menu.inputMenu()) break;
+      }
+    } else {
+      logger.error("Failed to establish connection.");
     }
+
+    ConnectionManager.closeConnection();
+  }
 }
